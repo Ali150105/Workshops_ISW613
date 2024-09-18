b7d2b05403293864fcf766f3de70a30edf034e40
Ali482-eng <131223770+Ali482-eng@users.noreply.github.com>
2024-09-17 19:00:46 -0600

    cambios

--- workshop_1/index.php
+++ workshop_1/index.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Current Date and Time</title>
-</head>
-<body>
-    <h1>Current Date and Time</h1>
-    <?php
-    // Set the default timezone to your desired timezone
-    date_default_timezone_set('America/New_York');
-    
-    // Get current date and time
-    $currentDateTime = date('Y-m-d H:i:s');
-    
-    // Print the current date and time
-    echo "<p>Current Date and Time: $currentDateTime</p>";
-    ?>
-</body>
-</html>
+<?php
+// Establece la zona horaria a 'America/New_York' 
+date_default_timezone_set('America/New_York');
+
+// Obtén la fecha y hora actual
+$currentDateTime = date('Y-m-d H:i:s');
+
+// Imprime la fecha y hora actual
+echo "Current Date and Time: $currentDateTime";
+?>
