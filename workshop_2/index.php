302fe0b3f651e2305d322f57dfa20acf88a0e697
Ali482-eng <131223770+Ali482-eng@users.noreply.github.com>
2024-09-24 18:18:42 -0600

    cambios de titulos de la pagina

--- workshop_2/index.php
+++ workshop_2/index.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Print Name</title>
+  <title>Personal Information</title>
 </head>
 <body>
 <form action="/workshop_2/save.php" method="POST">
